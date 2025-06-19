690e4e8acea03fffa8f87266642b897d502d613c
KrrishChakravarty <kchakravartykrrish@gmail.com>
2025-06-19 17:27:07 +0530

    main-menu

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // function gets executed when the button is pressed.
+    public void changeScene()
+    {
+        // write the code to change the scene
+        Debug.Log("Button Pressed!");
+    }
+}
+    
+
