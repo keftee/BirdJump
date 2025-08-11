fde3f0f66fe7b47bd592a23b220e0d255f8ac471
KrrishChakravarty <kchakravartykrrish@gmail.com>
2025-08-11 15:46:13 +0530

    game-over and proper nav

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -6,49 +6,40 @@ using System.Numerics;
 public class GameLogic : MonoBehaviour
 {
     public int score;
+    private bool gameover;
     [SerializeField] TMP_Text scoreCard;
     [SerializeField] UnityEngine.UI.Image pausemenu;
+    [SerializeField] UnityEngine.UI.Image gameovermenu;
     BackgroundMoveScript[] movnigObjects;
     [SerializeField] GameObject player;
     [SerializeField] private float jumpForce = 2.0f;
-    [SerializeField] private float torque = 0.0f;
-    [SerializeField] private float gravityTorque = -1.0f;
+    // [SerializeField] private float torque = 0.0f;
+    // [SerializeField] private float gravityTorque = 0f;
     private Dictionary<BackgroundMoveScript, float> savedSpeeds = new Dictionary<BackgroundMoveScript, float>();
-    UnityEngine.Vector3 bottomLimit = new UnityEngine.Vector3(0,-4.5f,-2);
+    UnityEngine.Vector3 bottomLimit = new UnityEngine.Vector3(0, -4.5f, -2);
+    UnityEngine.Vector3 topLimit = new UnityEngine.Vector3(0, 5.0f, -2);
     PolygonCollider2D playerCollider;
 
     Rigidbody2D playerRB;
     void Start()
     {
+        gameover = false;
         playerCollider = player.GetComponent<PolygonCollider2D>();
         playerRB = player.GetComponent<Rigidbody2D>();
         Application.targetFrameRate = 60;
         score = 0;
         pausemenu.gameObject.SetActive(false); // Hide pause menu at start
+        gameovermenu.gameObject.SetActive(false); // Hide pause menu at start
     }
     void Update()
     {
-        if (player.transform.position.y < bottomLimit.y)
+        if (player.transform.position.y < bottomLimit.y || player.transform.position.y > topLimit.y)
         {
             //game over screen
-            //just restart the level for now
-            restartScene();
+            gameOver();
         }
-        if (Input.GetMouseButtonDown(0))
-        {
-            // playerRB.freezeRotation = false;
-            playerRB.AddForceY(jumpForce, ForceMode2D.Impulse);
-            // playerRB.AddTorque(torque, ForceMode2D.Impulse);
-        }
-        if (!Input.GetMouseButtonDown(0))// && player.transform.rotation.z > -0.5f)
-        {
-            playerRB.AddTorque(gravityTorque * -1, ForceMode2D.Force);
-            // playerRB.AddTorque(gravityTorque*-(1-(player.transform.rotation.z/-0.5f)), ForceMode2D.Force);
-        }
-        if (player.transform.rotation.z < -0.2  f)
-        {
-            playerRB.freezeRotation = true;
-        }
+
+        // check for collisions
         int lavaLayer = LayerMask.NameToLayer("Lava");
         ContactFilter2D contactFilter = new ContactFilter2D();
         contactFilter.SetLayerMask(1 << lavaLayer);
@@ -58,10 +49,19 @@ public class GameLogic : MonoBehaviour
         int hitCount = playerCollider.Overlap(contactFilter, results);
         if (hitCount > 0)
         {
-            restartScene();
+            gameOver();
         }
 
+        if (Input.GetMouseButtonDown(0) && !gameover)
+        {
+            // playerRB.freezeRotation = false;
+            playerRB.AddForceY(jumpForce, ForceMode2D.Impulse);
+            // playerRB.AddTorque(torque, ForceMode2D.Impulse);
+        }
     }
+
+    // private void FixedUpdate() {
+    // }
 
     public void changeScoreCard()
     {
@@ -89,8 +89,24 @@ public class GameLogic : MonoBehaviour
         pausemenu.gameObject.SetActive(false);
     }
 
+    public void gameOver()
+    {
+        gameover = true;
+        movnigObjects = FindObjectsByType<BackgroundMoveScript>(sortMode: FindObjectsSortMode.None);
+        foreach (BackgroundMoveScript instance in movnigObjects)
+        {
+            instance.speed = 0f;
+        }
+        gameovermenu.gameObject.SetActive(true);
+    }
+
     public void restartScene()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void mainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
     }
 }
